070f5d7564480ed44b5db65e6b535b059e151c39
coder7475 <robiulhossain7475@gmail.com>
2025-06-24 07:02:26 +0600

    feat: write a function

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
 class Program {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
   public static void Main() {
-    Console.WriteLine("Hello World!!");
+    Console.WriteLine("Hello from c#!!");
+    Console.WriteLine("Sum of 2 + 4: {0}", sum(2, 4));
   }
 }
