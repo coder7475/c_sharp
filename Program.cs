e63bc42ec1ba7390fd56a555b2cafb59fbd76f68
coder7475 <robiulhossain7475@gmail.com>
2025-06-28 13:07:46 +0600

    feat: learn inheritence

--- Program.cs
+++ Program.cs
@@ -2,16 +2,29 @@
 
 // OOP 
 // Student Management System
+class Auth {
+  public bool IsAuthenticated() {
+    return false;
+  }
+}
+
 class Student {
   private string name;
   private int age;
+  Auth auth = new Auth();
   
   public Student(string name, int age) {
     this.name = name;
     this.age = age;
   }
   public void SetName(string name) {
-    this.name = name;
+    if (auth.IsAuthenticated()){   
+      this.name = name;
+    }
+    else
+    {
+        Console.WriteLine("You are not Authenticated");
+    }
   }
 
   public string GetName() {
@@ -20,6 +33,16 @@ class Student {
 
   public void ShowInfo() {
     Console.WriteLine("Name: {0} Age: {1}", name, age);
+  }
+}
+
+// Inheritence
+class JIPCStudent : Student {
+  public JIPCStudent(string name, int age): base(name, age) {
+
+  }
+  public void GiveQuiz() {
+    Console.WriteLine("Give Quiz");
   }
 }
 
@@ -32,5 +55,10 @@ class Program {
     student.SetName("Shahriar");
     student.ShowInfo();
     Console.WriteLine(student.GetName());
+
+
+    JIPCStudent jipcStudent = new JIPCStudent("John", 20);
+    jipcStudent.ShowInfo();
+    jipcStudent.GiveQuiz();
   }
 }
