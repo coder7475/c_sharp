2380b02d76b611e54f8c8215d1476f7cc1999855
coder7475 <robiulhossain7475@gmail.com>
2025-07-07 11:28:10 +0600

    feat: archive class 2

---
+++ archive/class2_Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// OOP 
+// Student Management System
+class Auth {
+  public bool IsAuthenticated() {
+    return false;
+  }
+}
+
+class Student {
+  private string name;
+  private int age;
+  Auth auth = new Auth();
+  
+  public Student(string name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+  public void SetName(string name) {
+    if (auth.IsAuthenticated()){   
+      this.name = name;
+    }
+    else
+    {
+        Console.WriteLine("You are not Authenticated");
+    }
+  }
+
+  public string GetName() {
+    return this.name;
+  }
+
+  public void ShowInfo() {
+    Console.WriteLine("Name: {0} Age: {1}", name, age);
+  }
+}
+
+// Inheritence
+class JIPCStudent : Student {
+  public JIPCStudent(string name, int age): base(name, age) {
+
+  }
+  public void GiveQuiz() {
+    Console.WriteLine("Give Quiz");
+  }
+}
+
+class Program2 {
+  public static void Main2() {   
+    Student student = new Student("Saif", 27);
+
+
+
+    student.SetName("Shahriar");
+    student.ShowInfo();
+    Console.WriteLine(student.GetName());
+
+
+    JIPCStudent jipcStudent = new JIPCStudent("John", 20);
+    jipcStudent.ShowInfo();
+    jipcStudent.GiveQuiz();
+  }
+}
